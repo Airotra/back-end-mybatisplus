a0ab4cd6f8a2584d7e77ceb42936fe16550e8c12
Airotra <Airotra@users.noreply.github.com>
2021-06-21 09:54:57 +0800

    2021.6.21，编写后端注册功能2

---
+++ src/main/java/com/example/mybatisplus/mapper/TrolleyMapper.java
@@ -0,0 +1,16 @@
+package com.example.mybatisplus.mapper;
+
+import com.example.mybatisplus.model.domain.Trolley;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author lxp
+ * @since 2021-06-21
+ */
+public interface TrolleyMapper extends BaseMapper<Trolley> {
+
+}
---
+++ src/main/java/com/example/mybatisplus/model/domain/Trolley.java
@@ -0,0 +1,38 @@
+package com.example.mybatisplus.model.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author lxp
+ * @since 2021-06-21
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="Trolley对象", description="")
+public class Trolley extends Model<Trolley> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "trolley_id",type= IdType.ASSIGN_ID )
+    private Long id;
+
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+}
