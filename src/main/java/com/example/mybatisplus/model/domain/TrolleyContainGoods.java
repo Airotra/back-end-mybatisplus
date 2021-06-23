7b7053a3d08027aa7f4a8277cb7b6f66b313f09e
MD <md@666.com>
2021-06-23 17:16:07 +0800

    购物车表

---
+++ src/main/java/com/example/mybatisplus/model/domain/TrolleyContainGoods.java
@@ -0,0 +1,48 @@
+package com.example.mybatisplus.model.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author lxp
+ * @since 2021-06-22
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="TrolleyContainGoods对象", description="")
+public class TrolleyContainGoods extends Model<TrolleyContainGoods> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "trolley_id",type= IdType.ASSIGN_ID )
+    private Long id;
+
+    private Long goodsId;
+
+    private Integer goodsNumber;
+
+    private String goodsName;
+
+    private Double goodsPrice;
+
+    private String goodsPicture;
+
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+}
