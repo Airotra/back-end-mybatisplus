0833846ffd589b6923a22ef01f3121f01538084f
Airotra <Airotra@users.noreply.github.com>
2021-06-23 09:54:54 +0800

    2021.6.23，编写收货地址管理后端2

---
+++ src/main/java/com/example/mybatisplus/model/domain/Adress.java
@@ -0,0 +1,52 @@
+package com.example.mybatisplus.model.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author lxp
+ * @since 2021-06-23
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="Adress对象", description="")
+public class Adress extends Model<Adress> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "adress_id",type= IdType.ASSIGN_ID )
+    private Long id;
+
+    @TableField(value = "user_id")
+    private Long userId;
+
+    private String nation;
+
+    private String provice;
+
+    private String city;
+
+    private String district;
+
+    private String addr;
+
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+}
