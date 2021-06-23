b531d1676f332fc8dbbe6ec0040f5005be538978
MD <md@666.com>
2021-06-23 17:15:47 +0800

    购物车相关函数

---
+++ src/main/java/com/example/mybatisplus/service/TrolleyContainGoodsService.java
@@ -0,0 +1,16 @@
+package com.example.mybatisplus.service;
+
+import com.example.mybatisplus.model.domain.TrolleyContainGoods;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lxp
+ * @since 2021-06-22
+ */
+public interface TrolleyContainGoodsService extends IService<TrolleyContainGoods> {
+
+}
