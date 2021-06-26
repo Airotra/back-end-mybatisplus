aee46fead42e4f87d7ed788673254b83e5062950
syan <1042752003@qq.com>
2021-06-26 19:00:12 +0800

    syan: picture updated

--- src/sql/2021-06-26.sql
+++ src/sql/2021-06-26.sql
@@ -1,2 +1,4 @@
 -- 修改trolleyContainGoods表的字符集为utf-8 以支持中文
 alter table trolley_contain_goods convert to character set utf8;
+-- 修改trolleyContainGoods表中的图片字段 长度到1024
+ALTER TABLE trolley_contain_goods MODIFY Column goods_picture varchar(1024) not null;
